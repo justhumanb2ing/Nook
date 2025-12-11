61551b16806b5a4a4395599d5bca67bd0bbe51ea
coggiee <zentechie7@gmail.com>
2025-12-11 19:36:02 +0900

    Remove unused Button import from block-editors.tsx to streamline component dependencies.

--- components/profile/block-editors.tsx
+++ components/profile/block-editors.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import type { HTMLAttributes } from "react";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { useLinkBlockEditor } from "@/hooks/use-link-block-editor";
